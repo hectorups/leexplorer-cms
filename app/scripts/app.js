1c946c83a367e8333c504d32106148079bce55f9
Hector Monserrate <hectormonserrate@Hectors-MacBook-Pro.local>
2014-12-18 19:52:58 -0800

    fix send cookies on CORS

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -36,11 +36,13 @@ angular
         redirectTo: '/login'
       });
 
+    // Cookies for CORS
+    $httpProvider.defaults.withCredentials = true;
     $httpProvider.interceptors.push(['$rootScope', '$q', '$location', 'SessionService', function($rootScope, $q, $location, SessionService) {
       return {
         responseError: function(rejection) {
           if (rejection.status === 401) {
-            SessionService.setLogged(false);
+            SessionService.destroy();
             $location.url('/login');
           }
           return $q.reject(rejection);
@@ -53,12 +55,15 @@ angular
     $rootScope.$on("$locationChangeStart", function (event, next) {
       if (!SessionService.isLogged()) {
         $location.url('/login');
-      } 
+      } else if(_.last(next.split('/')) == 'login') {
+        $location.url('/home');
+      }
     });
 
     /* TODO: apparently the location declaration on the rootscope is needed so that the nav on the 
             index.html knows it's defined (since it has no controller). How can I organize app so that
             nav can exist with it's own controller while coexisting with the ng-view? */
+    $rootScope.$location = $location;
     $rootScope.AuthService = AuthService;
     $rootScope.SessionService = SessionService;
   });
