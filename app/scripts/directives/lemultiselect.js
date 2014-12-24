26b4d301369fa00620a78cb4294bceffb04d7167
Hector Monserrate <hectormonserrate@Hectors-MacBook-Pro.local>
2014-12-24 13:51:19 -0800

    refactor multiselect

--- app/scripts/directives/lemultiselect.js
+++ app/scripts/directives/lemultiselect.js
@@ -10,57 +10,51 @@ angular.module('leexplorerFrontendApp')
   .directive('leMultiselect', ['$translate', '$compile', function ($translate, $compile) {
     return {
       restrict: 'E',
+      template: '<div ng-dropdown-multiselect="" options="facilities" selected-model="selectedValues" extra-settings="settings" events="events"></div>',
       scope: {
         'currentValues': '=',
         'values': '=',
         'translationPrefix': '='
       },
-      link: function postLink(scope, element, attrs) {
+      controller: function($scope, $translate, $compile) {
+        var convertValues = function(values) {
+          return _.map(values, function(item){
+            return {id: item};
+          });
+        }
 
-        scope.convertValues = function(values) {
-          var selectedValues = [];
-          _.each(values,function(item){
-            selectedValues.push({id: item});
-          }); 
-
-          return selectedValues;
-        };
+        var selectedValues = convertValues($scope.values);
         
-        scope.settings = {'showCheckAll': false, 'showUncheckAll': false};
-        scope.selectedValues = scope.convertValues(scope.currentValues); 
+        $scope.settings = {'showCheckAll': false, 'showUncheckAll': false};
+        $scope.selectedValues = convertValues($scope.currentValues); 
 
-        var translation_values = _.map(scope.values, function(value) { 
-          return {id: value, label: scope.translationPrefix + value.toUpperCase()} 
+        var translation_values = _.map($scope.values, function(value) { 
+          return {id: value, label: $scope.translationPrefix + value.toUpperCase()} 
         });
         $translate(_.map(translation_values,function(value) {return value.label})).then(function(translations){
-          scope.facilities = _.map(translation_values, function(value) {
+          $scope.facilities = _.map(translation_values, function(value) {
             value.label = translations[value.label];
             return value;
           });
         });
 
-        scope.$watch(scope.currentValues, function(newVal, oldVal) {
+        $scope.$watch($scope.currentValues, function(newVal, oldVal) {
           if(_.isArray(newVal)) {
-            scope.selectedValues = scope.convertValues(newVal); 
+            $scope.selectedValues = convertValues(newVal); 
           }
         });
 
-        scope.events = {
+        $scope.events = {
           onItemSelect: function(item) {
             if(item) {
-              scope.currentValues.push(item.id);
+              $scope.currentValues.push(item.id);
             }
         }, onItemDeselect: function(item) {
             if(item) {
-              var pos = scope.currentValues.indexOf(item.id);
-              scope.currentValues.splice(pos, 1);
+              var pos = $scope.currentValues.indexOf(item.id);
+              $scope.currentValues.splice(pos, 1);
             }
         }};
-
-        element.append('<div ng-dropdown-multiselect="" options="facilities" selected-model="selectedValues" extra-settings="settings" events="events"></div>');
-        // we need to tell angular to render the directive
-        $compile(element.contents())(scope);
-
       }
     };
   }]);
